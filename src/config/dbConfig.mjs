0fad47aec98157536cde70adaf8d02566aa0d510
Florencia Pedraza Aguero <florenciapedrazaaguero@gmail.com>
2025-11-14 23:10:48 -0300

    Agregar ejercicios trabajo práctico 1

---
+++ src/config/dbConfig.mjs
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+    try {
+        await mongoose.connect('mongodb+srv://Grupo-15:grupo15@cursadanodejs.ls9ii.mongodb.net/Node-js');
+        console.log('Conexión exitosa a MongoDB');
+    } catch (error) {
+        console.error('Error al conectar a MongoDB:', error);
+        process.exit(1);
+    }
+}
